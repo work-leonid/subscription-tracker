9af95863107d07e42ca95eb36cb6ebba61477420
work-leonid <work.leonid@gmail.com>
2021-10-03 11:52:14 +0300

    feat: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+subscriptions = Subscription.create([
+                                      { name: 'Apple Music',
+                                        price: 199,
+                                        cycle: 'month',
+                                        currency: 'rub',
+                                        reminder: '2021-10-21',
+                                        trial: false,
+                                        subscription_type: 'Family',
+                                        bill_date: '2021-10-10',
+                                        duration: 'month' },
+                                      { name: 'YouTube Premium',
+                                        price: 258,
+                                        cycle: 'month',
+                                        currency: 'rub',
+                                        reminder: '2021-10-21',
+                                        trial: false,
+                                        subscription_type: 'Individual',
+                                        bill_date: '2021-10-10',
+                                        duration: 'month' },
+                                      { name: 'Mobile',
+                                        price: 399,
+                                        cycle: 'month',
+                                        currency: 'rub',
+                                        reminder: '2021-10-21',
+                                        trial: false,
+                                        subscription_type: 'Individual',
+                                        bill_date: '2021-10-10',
+                                        duration: 'month' },
+                                    ])
